4887bc61dda9588c4af9c6a5657c1172fef215af
CrossroadsMan <crossroadsman@gmail.com>
2018-08-16 01:12:18 -0600

    Start constructing views

--- mineral_catalog/catalog/urls.py
+++ mineral_catalog/catalog/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path
 
 from . import views
 
+
+app_name='catalog'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path('^$', views.index, name='index'),
-    path('catalog/', include('catalog.urls', namespace='catalog')),
+    path('', views.index, name='index'),
 ]
