d86aeca68d7884c335247cffa2ba27d4fa328316
CrossroadsMan <crossroadsman@gmail.com>
2018-08-16 13:09:13 -0600

    Start replicating example views

--- mineral_catalog/catalog/views.py
+++ mineral_catalog/catalog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from .models import Mineral
+
 
 def index(request):
+    minerals = Mineral.objects.all()
     template = 'catalog/index.html'
-    return render(request, template)
+    context = {'minerals': minerals}
+    return render(request, template, context)
+
+def detail(request, mineral_id):
+    mineral = Mineral.objects.get(pk=mineral_id)
+    template = 'catalog/detail.html'
+    context = {'mineral': mineral}
+    return render(request, template, context)
